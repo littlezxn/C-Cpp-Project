2acfe89acfc7773ca0a1723e8fa50f1a2ad42bd2
littlezyn <zaynproton001@proton.me>
2026-03-19 00:55:51 +0530

    Adding Factorial.c

---
+++ C/Factorial/Factorial.c
@@ -0,0 +1,36 @@
+#include<stdio.h> //headerfile for input and output
+int Factorial(int n){
+    if (n == 1 || n == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return n * Factorial(n - 1);
+    }   
+}
+int main(){
+    int x;
+    printf("Enter the number you want the factorial of: ");
+    scanf("%d", &x);
+    x = Factorial(x);
+    printf("%d", x);
+    return 0;
+}
+//end of the program
+// output
+/*
+Enter the number you want the factorial of: 5
+120
+*/
+
+//how it works
+/*
+/*
+factorial of 5 will be
+5 * Factorial(4)
+5 * 4 * Factorial(3)
+5 * 4 * 3 * Factorial(2)
+5 * 4 * 3 * 2 * Factorial(1)
+5 * 4 * 3 * 2 * 1
+*/
