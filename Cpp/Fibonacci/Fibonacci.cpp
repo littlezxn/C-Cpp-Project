72e4c29350a54da8938aefd3607fb221f97fd7e5
littlezxn <littlezyn@proton.me>
2026-03-29 21:38:17 +0530

    Enhance comments and formatting in Fibonacci program

--- Cpp/Fibonacci/Fibonacci.cpp
+++ Cpp/Fibonacci/Fibonacci.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <iostream> //headerfile for Input/Output stream
 using namespace std;
 int main()
 {
@@ -8,11 +8,17 @@ int main()
     cout << "Fibonacci Series: ";
     for (int i = 0; i < n; i++)
     {
-        cout << a << " ";
+        cout << a << " "; //first it prints a then it gives space and then next number;
         next = a + b;
         a = b;
         b = next;
     }
-    cout << endl;
+    cout << endl; //after ending the loop it will give a new line
     return 0;
 }
+//end of the program
+/*
+Example of 5
+Enter number of terms: 5
+Fibonacci Series: 0 1 1 2 3 
+*/
