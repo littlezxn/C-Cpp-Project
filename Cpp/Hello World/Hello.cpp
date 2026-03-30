ed60b350df60450300ca800f981541d071a21bbc
littlezxn <littlezyn@proton.me>
2026-03-30 23:59:09 +0530

    Fix comments and typo in Hello.cpp

--- Cpp/Hello World/Hello.cpp
+++ Cpp/Hello World/Hello.cpp
@@ -1,7 +1,7 @@
 #include<iostream> //Headerfile for inpurt and output
 using namespace std;
 int main(){
-    cout<<"Hello World!"<<endl;
+    cout<<"Hello World!"<<endl; //end of line
     return 0;
 }
-//end of teh program
+//end of the program
