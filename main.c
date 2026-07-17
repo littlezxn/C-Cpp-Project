8ff8c4a8129c8e0ef67ef3ef0e542ea965b4e655
littlezxn <littlezyn@proton.me>
2026-07-17 22:19:38 +0530

    Add main program with menu and language choice functionality

---
+++ main.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdlib.h>
+void displayMenu();
+void choices();
+int main(){
+    displayMenu();
+    choices();
+    return 0;
+}
+void displayMenu()
+{
+    printf("Welcome to C/C++ programming!\n");
+    printf("\n");
+    printf("Which language you want to learn?\n");
+    printf("1 for C\n2 for C++\n");
+}
+void choices()
+{
+    int choice;
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
+    switch(choice)
+    {
+    case 1:
+        printf("You have chosen to learn C programming.\n");
+        system("cd C && ls || dir");
+        break;
+    case 2:
+        printf("You have chosen to learn C++ programming.\n");
+        system("cd Cpp && ls || dir");
+        break;
+    default:
+        printf("Invalid choice.\n");
+    }
+}
