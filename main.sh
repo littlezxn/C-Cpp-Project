40185fb23e61c25065a2ecba96f29c592cfe3335
littlezxn <littlezyn@proton.me>
2026-07-27 22:39:20 +0530

    Refactor main.sh to improve user prompts and error handling for script execution

--- main.sh
+++ main.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
-echo "Current directory:"
-pwd
-ls
-read -p "Enter your choice (c/c++): " choice
-if [ "$choice" == "c" ]; then
-    cd "C"
-    ls -l
-elif [ "$choice" == "c++" ]; then
-    cd "Cpp"
-    ls -l
-elif [ "$choice" == "main.c" ]; then
-    cat main.c
-elif [ "$choice" == "main.sh" ]; then
-    cat main.sh
+if [ "$EUID" -ne 0 ]; then
+    echo -e "\033[0;32mSuccess: The script is NOT running with sudo privileges.\033[0m"
+    echo "Current directory:"
+    pwd
+    ls
+    read -p "Enter your choice (c/c++), "x" for exit: " choice
+    if [ "$choice" == "c" ]; then
+        cd "C"
+        ls -l
+    elif [ "$choice" == "c++" ]; then
+        cd "Cpp"
+        ls -l
+    elif [ "$choice" == "main.c" ]; then
+        cat main.c
+    elif [ "$choice" == "main.sh" ]; then
+        cat main.sh
+    elif [ "$choice" == "x" ]; then
+        echo "Exiting the script."
+        exit 0
+    else
+        echo "Invalid choice"
+    fi
 else
-    echo "Invalid choice"
+    echo -e "\033[0;31mError: This script cannot be run with sudo!\033[0m" >&2
+    exit 1
 fi
+
